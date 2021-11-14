dab1a1b35949d9179f34e3062bb550074df0b9e5
Tetiana Oprysk <tetyana.oprisk@gmail.com>
2021-11-14 22:31:51 +0300

    Deleted github_token field

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,5 @@ from wtforms.validators import Length, DataRequired
 
 
 class Form(FlaskForm):
-    github_token = StringField(label='GitHub Token:', validators=[Length(min=2, max=30), DataRequired()])
     github_login = StringField(label='GitHub Login:', validators=[Length(min=2, max=30), DataRequired()])
     submit = SubmitField(label='Search')
